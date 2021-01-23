ad9855cd1856f3860682edd81ca4a54dc4d65e82
Baez2222 <baez.hector33@gmail.com>
2021-01-23 15:54:13 -0600

    moved script to js file

--- taqueriaposapp/static/js/main.js
+++ taqueriaposapp/static/js/main.js
@@ -12,3 +12,26 @@ document.getElementById("getTime").innerHTML = time;
 function addNumber(element) {
     document.getElementById('keypadVar').value = document.getElementById('keypadVar').value + element.value;
 }
+
+function changeTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    for (i=0; i < document.getElementsByClassName(tabName).length; i++){
+      document.getElementsByClassName(tabName)[i].style.display = "block";
+      evt.currentTarget.className += " active";
+    }
+}
